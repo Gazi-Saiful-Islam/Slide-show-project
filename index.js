313133413535186de1cda1e98920792b040b790d
Gazi-Saiful-Islam <gazisaifulislam894@gmail.com>
2023-12-10 02:28:15 +0300

    it is for slide-show project

---
+++ index.js
@@ -0,0 +1,61 @@
+let slideShowElements=document.querySelectorAll(".slide-show-element")
+
+let count = 1;
+setInterval(() => {
+  count++;
+  let currentElement = document.querySelector(".current")
+  currentElement.classList.remove("current")
+ 
+
+if(count>slideShowElements.length){
+  slideShowElements[0].classList.add("current")
+  count=1;
+}else{
+  currentElement.nextElementSibling.classList.add("current")
+  
+}
+}, 2000);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
